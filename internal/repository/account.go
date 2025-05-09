b260175c7ec6c412cce648a56065458c3b72ca34
kodaimura <kodaimura123@gmail.com>
2025-05-10 00:47:07 +0900

    a -> m

--- internal/repository/account.go
+++ internal/repository/account.go
@@ -7,15 +7,15 @@ import (
 )
 
 type AccountRepository interface {
-	Get(a *model.Account) ([]model.Account, error)
-	GetOne(a *model.Account) (model.Account, error)
-	GetAll(a *model.Account) ([]model.Account, error)
+	Get(m *model.Account) ([]model.Account, error)
+	GetOne(m *model.Account) (model.Account, error)
+	GetAll(m *model.Account) ([]model.Account, error)
 
-	Insert(a *model.Account) (model.Account, error)
-	Update(a *model.Account) (model.Account, error)
-	Delete(a *model.Account) error
+	Insert(m *model.Account) (model.Account, error)
+	Update(m *model.Account) (model.Account, error)
+	Delete(m *model.Account) error
 
-	InsertTx(a *model.Account, tx *gorm.DB) (model.Account, error)
-	UpdateTx(a *model.Account, tx *gorm.DB) (model.Account, error)
-	DeleteTx(a *model.Account, tx *gorm.DB) error
+	InsertTx(m *model.Account, tx *gorm.DB) (model.Account, error)
+	UpdateTx(m *model.Account, tx *gorm.DB) (model.Account, error)
+	DeleteTx(m *model.Account, tx *gorm.DB) error
 }
--- internal/repository/impl/account.go
+++ internal/repository/impl/account.go
@@ -14,50 +14,50 @@ func NewGormAccountRepository(db *gorm.DB) *gormAccountRepository {
 	return &gormAccountRepository{db: db}
 }
 
-func (rep *gormAccountRepository) Get(a *model.Account) ([]model.Account, error) {
+func (rep *gormAccountRepository) Get(m *model.Account) ([]model.Account, error) {
 	var accounts []model.Account
-	err := rep.db.Find(&accounts, a).Error
+	err := rep.db.Find(&accounts, m).Error
 	return accounts, handleGormError(err)
 }
 
-func (rep *gormAccountRepository) GetOne(a *model.Account) (model.Account, error) {
+func (rep *gormAccountRepository) GetOne(m *model.Account) (model.Account, error) {
 	var account model.Account
-	err := rep.db.First(&account, a).Error
+	err := rep.db.First(&account, m).Error
 	return account, handleGormError(err)
 }
 
-func (rep *gormAccountRepository) GetAll(a *model.Account) ([]model.Account, error) {
+func (rep *gormAccountRepository) GetAll(m *model.Account) ([]model.Account, error) {
 	var accounts []model.Account
-	err := rep.db.Unscoped().Find(&accounts, a).Error
+	err := rep.db.Unscoped().Find(&accounts, m).Error
 	return accounts, handleGormError(err)
 }
 
-func (rep *gormAccountRepository) Insert(a *model.Account) (model.Account, error) {
-	err := rep.db.Create(a).Error
-	return *a, handleGormError(err)
+func (rep *gormAccountRepository) Insert(m *model.Account) (model.Account, error) {
+	err := rep.db.Create(m).Error
+	return *m, handleGormError(err)
 }
 
-func (rep *gormAccountRepository) Update(a *model.Account) (model.Account, error) {
-	err := rep.db.Save(a).Error
-	return *a, handleGormError(err)
+func (rep *gormAccountRepository) Update(m *model.Account) (model.Account, error) {
+	err := rep.db.Save(m).Error
+	return *m, handleGormError(err)
 }
 
-func (rep *gormAccountRepository) Delete(a *model.Account) error {
-	err := rep.db.Delete(a).Error
+func (rep *gormAccountRepository) Delete(m *model.Account) error {
+	err := rep.db.Delete(m).Error
 	return handleGormError(err)
 }
 
-func (rep *gormAccountRepository) InsertTx(a *model.Account, tx *gorm.DB) (model.Account, error) {
-	err := tx.Create(a).Error
-	return *a, handleGormError(err)
+func (rep *gormAccountRepository) InsertTx(m *model.Account, tx *gorm.DB) (model.Account, error) {
+	err := tx.Create(m).Error
+	return *m, handleGormError(err)
 }
 
-func (rep *gormAccountRepository) UpdateTx(a *model.Account, tx *gorm.DB) (model.Account, error) {
-	err := tx.Save(a).Error
-	return *a, handleGormError(err)
+func (rep *gormAccountRepository) UpdateTx(m *model.Account, tx *gorm.DB) (model.Account, error) {
+	err := tx.Save(m).Error
+	return *m, handleGormError(err)
 }
 
-func (rep *gormAccountRepository) DeleteTx(a *model.Account, tx *gorm.DB) error {
-	err := tx.Delete(a).Error
+func (rep *gormAccountRepository) DeleteTx(m *model.Account, tx *gorm.DB) error {
+	err := tx.Delete(m).Error
 	return handleGormError(err)
 }
