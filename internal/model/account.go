a1766bdd633279769ce0b0ed82bad9c3c2ae63f0
kodaimura <kodaimura123@gmail.com>
2025-05-19 20:37:44 +0900

    fix model

--- internal/model/account.go
+++ internal/model/account.go
@@ -7,10 +7,10 @@ import (
 )
 
 type Account struct {
-	Id        int            `db:"id" json:"id" gorm:"primaryKey;autoIncrement"`
-	Name      string         `db:"name" json:"name"`
-	Password  string         `db:"password" json:"password"`
-	CreatedAt time.Time      `db:"created_at" json:"created_at" gorm:"column:created_at"`
-	UpdatedAt time.Time      `db:"updated_at" json:"updated_at" gorm:"column:updated_at"`
-	DeletedAt gorm.DeletedAt `db:"deleted_at" json:"deleted_at" gorm:"index"`
+	Id        int            `db:"id" gorm:"column:id;primaryKey;autoIncrement"`
+	Name      string         `db:"name" gorm:"column:name"`
+	Password  string         `db:"password" gorm:"column:password"`
+	CreatedAt time.Time      `db:"created_at" gorm:"column:created_at"`
+	UpdatedAt time.Time      `db:"updated_at" gorm:"column:updated_at"`
+	DeletedAt gorm.DeletedAt `db:"deleted_at" gorm:"column:deleted_at;index"`
 }
